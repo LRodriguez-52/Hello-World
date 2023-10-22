85ecafeea33ed315552f619c90fa965e1256d2c7
LRodriguez-52 <147327291+LRodriguez-52@users.noreply.github.com>
2023-10-22 17:49:51 -0500

    Python Debugging

---
+++ L.Rodriguez-Time.py
@@ -0,0 +1,8 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt)
---
+++ L.Rodriguez-Time2.py
@@ -0,0 +1,7 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of hours to wait?")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
